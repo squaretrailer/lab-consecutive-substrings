f12b74921cd190b3a5191fb03dd73633228b15ef
Christian <your_email@example.com>
2026-04-17 10:31:46 +0300

    feat:got all strings

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,28 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      result.push(string.slice(i, j + 1));
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
   console.log("");
-
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(''));
+
+  console.log("");
+  console.log("Expecting: ['a', 'ab', 'abc', 'abcd', 'b', 'bc', 'bcd', 'c', 'cd', 'd']");
+  console.log("=>", consecutiveSubstrings('abcd'));
 }
 
 module.exports = consecutiveSubstrings;
-
-
